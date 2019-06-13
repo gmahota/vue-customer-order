cd32499ece1afe9281f83f5d78a1d02b12699c32
Guimaraes Mahota <guimaraesmahota@gmail.CO.MZ>
2019-06-13 22:48:16 +0200

    add calendar GanttElastic

--- pages/Reservas/index.vue
+++ pages/Reservas/index.vue
@@ -1,31 +1,16 @@
 <template>
   <div class="container">
+    <h1> Reservas Hotel</h1>
 
-    <gantt :tasks="tasks"></gantt>
-
+    <no-ssr>
+    <Gantt/>  </no-ssr>
   </div>
 </template>
 
 <script>
-
 export default {
-  components : {
-	'gantt':  () => import('@/components/Gantt.vue')
-},
-  data () {
-    
-    return {
-        title: "Index",
-      tasks: {
-        data: [
-          {id: 1, text: 'Task #1', start_date: '15-06-2019', duration: 3, progress: 0.6},
-          {id: 2, text: 'Task #2', start_date: '18-06-2019', duration: 3, progress: 0.4}
-        ],
-        links: [
-          {id: 1, source: 1, target: 2, type: '0'}
-        ]
-      },
-    }
+  components: {
+    Gantt: () => import("@/components/calendar/GanttElastic.vue")
   }
-}
+};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,41 +10,6 @@
 
     <social />
   </div>
-
-
-  <!-- <v-container grid-list-md text-xs-center>
-    <v-layout align-center justify-center row fill-height>
-
-      <v-flex xs12>
-        <v-carousel>
-          <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src">
-
-          </v-carousel-item>
-        </v-carousel>
-      </v-flex>
-      <v-flex xs12>
-        <v-card flat>
-              <v-card-text>
-                <v-layout align-center mb-3>
-                  <v-avatar color="grey" class="mr-3"></v-avatar>
-                  <strong class="title">BEM VINDO AO AGNUS CRM</strong>
-                  <v-spacer></v-spacer>
-                  <v-btn icon>
-                    <v-icon>mdi-account</v-icon>
-                  </v-btn>
-                </v-layout>
-
-                <p>Nos dias de hoje, as Organizações procuram integração, automatização e mobilidade nos processos de negócio. </p>
-
-                <p>A integração, automatização e amobilidade tornam-se aspectos estratégicos para reduzir custos, complexidade e tempo, assim como para aumentar a produtividade.</p>
-
-                <p>A satisfação do cliente e a performance de TI. Com estas questões em mente, apresentamos os nossos produtos.</p>
-              </v-card-text>
-            </v-card>
-
-      </v-flex>
-    </v-layout>
-  </v-container> -->
 </template>
 
 <script>
