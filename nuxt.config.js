9136a36a154cf8e6efd945718ada2526eb30819c
gmahota <guimaraesmahota@gmail.com>
2024-05-09 22:20:46 +0200

    migrate to nuxt 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,63 +1,27 @@
-const nodeExternals = require('webpack-node-externals')
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
-module.exports = {
+export default defineNuxtConfig({
   /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'Agnus CMS',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
-    ]
+   ** Build configuration
+   */
+  build: {
+    transpile: ["vuetify"],
   },
-  plugins: ['~/plugins/vuetify.js'],
-  
-  css: ['~/assets/style/app.styl'],
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
 
   modules: [
-    '@nuxtjs/style-resources',
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
-    transpile: [/^vuetify/],
-    plugins: [
-      new VuetifyLoaderPlugin()
-    ],
-    extractCSS: true,
-    extend (config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-      if (process.server) {
-        config.externals = [
-          nodeExternals({
-            whitelist: [/^vuetify/]
-          })
-        ]
-      }
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
     }
-  },
+  ],
 
-  generate: {
-    
-  }
-}
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+});
